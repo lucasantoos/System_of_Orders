14ad595b5c72d389521748dfa24379cbf52ae385
Luc4sS1lv4 <lucas.lukas3245@gmail.com>
2025-10-16 14:01:44 -0400

    sucessy finelly

--- dist/Http/routers/main.Router.js
+++
@@ -1,5 +0,0 @@
-import { produtoController, orderController } from "../../main.js";
-export function Routers(fastify) {
-    fastify.post("/orders", orderController.createOrder);
-    fastify.post("/products", produtoController.createProductService);
-}
